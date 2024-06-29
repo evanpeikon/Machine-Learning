3d2e6479dfb4421db2ed0129fb538a3fc260ce8a
evanpeikon <154470963+evanpeikon@users.noreply.github.com>
2024-06-29 08:23:12 -0400

    Create classification-report.py

---
+++ Classification-Metrics/code/classification-report.py
@@ -0,0 +1,28 @@
+# Import libraries
+from pandas import read_csv
+from sklearn.model_selection import tran_test_split
+from sklearn.model_selection mport LogisticRegression
+from sklearn.linear_model import classification_report
+
+# Load data
+filename = 'diabetes.csv'
+names = ['pregnancies', 'glucose', 'DBP', 'skinfold','insulin','BMI','diabetes_pedigree','age','outcome']
+data = read_csv(filename, names=names)
+
+# Create array w/ data and seperate array into input/outputs
+array = data.values # inputs = all rows in columns 0-7
+x = array[:,0:8] # output = all rows in column 8
+y= array[:,8]
+
+# Create train-test split
+test_split = 0.35 # Sets the test split's size to 35% of dataset
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=test_split, random_state=seed)
+
+# Create and fit model
+model = LogisticRegression(solver= 'liblinear')
+model.fit(x_train, y_train)
+
+# Make predictions and generate classification report
+predictions= model.predict(x_test)
+CR = classification_report(y_test, predictions)
+print(CR)
